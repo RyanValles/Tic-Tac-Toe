6d4685ce5882bed6de37125f61bb50e584bf6092
Ryan <ravalles10@gmail.com>
2023-01-17 15:28:24 +0900

    Complete all functions for game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const cell = document.querySelectorAll(".cell")
+let cell = document.querySelectorAll(".cell")
 
 
 const gameboard = (() => {
@@ -8,93 +8,100 @@ const gameboard = (() => {
             board.push(cell[i])
      }
 
-   //players
-    const players = (name, mark, turn) => {
-        return {name, mark, turn}
+    const players = (name, mark) => {
+        return {name, mark}
     }
-    const player1 = players('Player 1', 'X')
-    const player2 = players('Player 2', 'O')
-    let activePlayer = player1
-
-    const display = document.querySelector('.display')
+    const player1 = players('Player X', 'X')
+    const player2 = players('Player O', 'O')
     
 
-     function switchPlayers(){
-        if (activePlayer === player1){
-         activePlayer = player2
-        }
-        else{
-         activePlayer = player1
-        }
-     }
-
-    // unremove cell events and clear cells,
-     //active player = player1
-     function reset(){
-        activePlayer = player1;
-        displayPlayer();
-        cell.forEach(cell => {
-            cell.textContent = ""
-        })
-
-    };
-  const resetBtn = document.querySelector(".reset");
-    resetBtn.onclick = reset;
-
-    //check winner if winner reset
-    const winningCombos =[
-        [0,1,2],[3,4,5],[6,7,8],[0,3,6],
-        [1,4,7],[2,5,8],[0,4,8],[2,4,6]
-    ]
-    const checkWinner = () =>{
-        winningCombos.forEach((element) => {
-            if (board[element[0]] === activePlayer.mark &&
-                 board[element[1]] === activePlayer.mark &&
-                 board[element[2]] === activePlayer.mark){
-                    //return this player wins and prevent any more clicks
-                    display.textContent = activePlayer.name + "wins!";
-
-                 }
-              
-    })}
-
+    const display = document.querySelector('.display')
     function displayPlayer(){
+        
         if (activePlayer === player1){
-            display.textContent = "Player X's turn"
-        }else if( activePlayer === player2){
-            display.textContent = "Player O's turn"
+            display.textContent = "Player X's Turn"
+        }else if(activePlayer === player2){
+            display.textContent = "Player O's Turn"
         }else{
             return;
        }
     }
+    let activePlayer = player1
+    let nonActivePlayer = player2
+     function switchPlayers(){
         
+        if (activePlayer === player1){
+       activePlayer = player2;
+       nonActivePlayer = player1;
+        
+        }
+        else if(activePlayer === player2){
+          activePlayer = player1
+          nonActivePlayer = player2;
+        }
+        
+      
+    };
+
+    // unremove cell events
+    function reset(){
+        activePlayer = player1;
+        displayPlayer();
+        cell.forEach((cell, index) => {
+            cell.textContent = ""
+            board[index] = ""
+            cell.removeAttribute
+        })
+    };
+    const resetBtn = document.querySelector(".reset");
+    resetBtn.onclick = reset;
+
+    const winningCombos =[
+        [0,1,2],[3,4,5],[6,7,8],[0,3,6],
+        [1,4,7],[2,5,8],[0,4,8],[2,4,6]
+    ]
+    function checkWinner(){
+        winningCombos.forEach((element) => {
+            if (board[element[0]] === activePlayer.mark &&
+                 board[element[1]] === activePlayer.mark &&
+                 board[element[2]] === activePlayer.mark){
+                    //prevent any more clicks
+                    display.textContent = nonActivePlayer.name + " wins!";
+                    cell.forEach(cell =>{
+                        if (cell.textContent !='X' || cell.textContent != 'O'){
+                            cell.textContent = ";)"
+                        }
+                    })
+                    
+             }       
+        });
+    }
+ 
+    
+    cell.forEach((cell, index) => {
+        cell.addEventListener('click', () => {
+            if(cell.textContent === 'X' || cell.textContent === 'O'
+            || cell.textContent === ';)'){
+                return
+            }else{
+            cell.textContent = activePlayer.mark; 
+            switchPlayers();displayPlayer();
+            board[index] = activePlayer.mark;
+            //remove event from cell
+             
+            checkWinner()}
+          
+        })
+    });
+  
     //check if all indexes equal a string then reset
 
 
-    return {board, switchPlayers, checkWinner, player1, player2
-    , displayPlayer, reset, activePlayer}
+    return {board, activePlayer, switchPlayers, checkWinner, player1, player2,
+        displayPlayer, reset}
 
 })();
 
-gameboard.displayPlayer()
-
-const gamecontrol = (() => { 
-//when cell clicked
-    cell.forEach(cell => {
-        cell.addEventListener('click', () => {
-            //display player
-            cell.textContent = gameboard.activePlayer.mark
-            gameboard.displayPlayer()
-            // update array value
-
-            //remove event from cell
-            
-            //if winner combo reset all
-           gameboard.checkWinner()
-            //if not next player true
-  
-})
-});
 
 
-})();
+
